34a89be8cae4698ef5ec075861c81b566563f649
Christine Hwang <chaehwang@college.harvard.edu>
2015-12-10 12:44:12 -0500

    fixed bug

--- js/companyScoreBreakdownVis.js
+++ js/companyScoreBreakdownVis.js
@@ -58,14 +58,14 @@ CompanyScoreBreakdownVis.prototype.wrangleData = function(){
     var thisData = [];
     var thisIndex = this.companyList.indexOf(this.company);
 
-    console.log(thisIndex);
+    console.log(this.company);
 
     if(thisIndex != -1){
 
         // map over the posts for this company
         this.data[thisIndex]['posts'].map(function(d, i){
-
             thisData.push(d.postSentimentScore);
+            console.log(d.postSentimentScore);
         })
 
         this.displayData = thisData;
@@ -91,10 +91,10 @@ CompanyScoreBreakdownVis.prototype.updateVis = function(){
     this.x = d3.scale.linear()
         .domain([d3.min(this.displayData), d3.max(this.displayData)])
         .range([0, that.width]);
-
+    console.log(this.displayData)
     // generate the histogram
     this.histogramData = d3.layout.histogram()
-                            .bins(that.x.ticks(20))
+                        
                             (this.displayData);
 
     console.log(this.histogramData);
